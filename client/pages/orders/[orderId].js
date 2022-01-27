1fbce93e8577eb457e9aa587f71c1a7e1c2bb59a
João Pedro Rosa <pedro300898@gmail.com>
2022-01-26 21:20:44 -0300

    client service

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push('/orders'),
+  });
+
+  const onPay = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <form onPay={onPay}>
+        <button className="btn btn-primary">Pay</button>
+      </form>
+      {errors}
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
